356fd02088ea89b1b9d390fec6cd7bb69c99692d
godjealous <hy8.8@163.com>
2014-08-03 16:05:43 +0800

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         vector<int> ret;
-        if(numbers.size()==0) return ret;
+        if(numbers.empty()) return ret;
         int n=numbers.size();
         map<int,int> mapping;
         for(int i=0;i<n;i++)
@@ -11,7 +11,7 @@ public:
         {
             if(mapping.find(target-numbers[i])!=mapping.end())
             {
-                if(i!=mapping[target-numbers[i]])
+                if(i!=mapping[target-numbers[i]])//a number can not be used twice
                 {
                     ret.push_back(min(i,mapping[target-numbers[i]])+1);
                     ret.push_back(max(i,mapping[target-numbers[i]])+1);
