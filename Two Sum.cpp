0560fd561e1355090df3de2fff96ea6aad04ba68
godjealous <hy8.8@163.com>
2014-08-03 15:07:04 +0800

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        vector<int> ret;
+        if(numbers.size()==0) return ret;
+        int n=numbers.size();
+        map<int,int> mapping;
+        for(int i=0;i<n;i++)
+            mapping[numbers[i]]=i;
+        for(int i=0;i<n;i++)
+        {
+            if(mapping.find(target-numbers[i])!=mapping.end())
+            {
+                if(i!=mapping[target-numbers[i]])
+                {
+                    ret.push_back(min(i,mapping[target-numbers[i]])+1);
+                    ret.push_back(max(i,mapping[target-numbers[i]])+1);
+                    break;
+                }
+                
+            }
+        }
+        return ret;
+    }
+};
